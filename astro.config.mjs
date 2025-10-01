cd297e1f1b1238dcb86fa25fa2214091d33372db
codificarlos <codificarlos@codificarloss-Mac-mini.local>
2025-10-01 17:42:10 -0600

    fix no open menu

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
 
   vite: {
-    plugins: [tailwindcss()]
+    plugins: []
   }
 });
